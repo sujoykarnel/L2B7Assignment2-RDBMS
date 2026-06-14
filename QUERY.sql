b34dc84bb856a729c8132814a3e43259a5255212
Sujoy Das <sujoykarnel@gmail.com>
2026-06-14 14:46:46 +0600

    Quary 4 done

--- QUERY.sql
+++ QUERY.sql
@@ -39,6 +39,11 @@ CREATE TABLE bookings (
 -- =========================================================================
 -- Query 1
 -- =========================================================================
-
 select match_id, fixture, base_ticket_price from matches
-where tournament_category like '%Champions League%' and  match_status = 'Available'
+where tournament_category = 'Champions League' and  match_status = 'Available'
+
+-- =========================================================================
+-- Query 2
+-- =========================================================================
+select user_id, full_name, email from users
+where full_name like 'Tanvir%' or full_name ilike '%Haque%'
