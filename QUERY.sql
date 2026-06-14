e7a65d7546d992ac46cc7c528cb59df182810a09
Sujoy Das <sujoykarnel@gmail.com>
2026-06-14 15:31:46 +0600

    Format Query

--- QUERY.sql
+++ QUERY.sql
@@ -4,22 +4,22 @@
 -- INSTRUCTIONS: Replace 'TYPE' and the constraint placeholders with your own
 --               actual data types, relational keys, and check criteria.
 -- =========================================================================
-
 -- DROP TABLES IF THEY ALREADY EXIST TO PREVENT CONFLICTS
 DROP TABLE IF EXISTS Bookings;
+
 DROP TABLE IF EXISTS Matches;
+
 DROP TABLE IF EXISTS Users;
 
 -- =========================================================================
 -- 1. CREATE USERS TABLE
 -- =========================================================================
 CREATE TABLE Users (
-    user_id serial primary key,
+    user_id serial PRIMARY KEY,
     full_name varchar(100),
-    email varchar(150) unique,
-    role varchar(50) check (role in ('Ticket Manager', 'Football Fan'),
+    email varchar(150) UNIQUE,
+    role varchar(50) CHECK (role IN ('Ticket Manager', 'Football Fan')),
     phone_number varchar(20)
-    
     -- Write your constraint to make 'user_id' the Primary Key
     -- Write your constraint to ensure 'email' values are never duplicated
     -- Write your check constraint to restrict 'role' to specific allowed strings
@@ -29,12 +29,18 @@ CREATE TABLE Users (
 -- 2. CREATE MATCHES TABLE
 -- =========================================================================
 CREATE TABLE Matches (
-    match_id serial primary key,
+    match_id serial PRIMARY KEY,
     fixture varchar(100),
     tournament_category varchar(100),
-    base_ticket_price decimal(10,2) check (base_ticket_price >= 0) ,
-    match_status varchar(20) check (match_status in ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
-    
+    base_ticket_price decimal(10, 2) CHECK (base_ticket_price >= 0),
+    match_status varchar(20) CHECK (
+        match_status IN (
+            'Available',
+            'Selling Fast',
+            'Sold Out',
+            'Postponed'
+        )
+    )
     -- Write your constraint to make 'match_id' the Primary Key
     -- Write your check constraint to prevent negative ticket prices
     -- Write your check constraint to restrict 'match_status' values
@@ -44,13 +50,14 @@ CREATE TABLE Matches (
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
 CREATE TABLE Bookings (
-    booking_id serial primary key,
-    user_id int references users(user_id),
-    match_id int references matches(match_id),
+    booking_id serial PRIMARY KEY,
+    user_id int REFERENCES users (user_id),
+    match_id int REFERENCES matches (match_id),
     seat_number varchar(20),
-    payment_status varchar(20) check (payment_status in ('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
-    total_cost decimal(10,2) check (total_cost >= 0)
-    
+    payment_status varchar(20) CHECK (
+        payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+    ),
+    total_cost decimal(10, 2) CHECK (total_cost >= 0)
     -- Write your constraint to make 'booking_id' the Primary Key
     -- Write your Foreign Key constraint linking 'user_id' to the Users table
     -- Write your Foreign Key constraint linking 'match_id' to the Matches table
@@ -58,37 +65,107 @@ CREATE TABLE Bookings (
     -- Write your check constraint to restrict 'payment_status' values
 );
 
-
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 -- =========================================================================
-INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
-(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
-(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
-(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
-(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+INSERT INTO
+    Users (user_id, full_name, email, role, phone_number)
+VALUES
+    (
+        1,
+        'Tanvir Rahman',
+        'tanvir@mail.com',
+        'Football Fan',
+        '+8801711111111'
+    ),
+    (
+        2,
+        'Asif Haque',
+        'asif@mail.com',
+        'Football Fan',
+        '+8801722222222'
+    ),
+    (
+        3,
+        'Sajjad Rahman',
+        'sajjad@mail.com',
+        'Ticket Manager',
+        '+8801733333333'
+    ),
+    (
+        4,
+        'Jannat Ara',
+        'jannat@mail.com',
+        'Football Fan',
+        NULL
+    );
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
 -- =========================================================================
-INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
-(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
-(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
-(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
-(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
-(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+INSERT INTO
+    Matches (
+        match_id,
+        fixture,
+        tournament_category,
+        base_ticket_price,
+        match_status
+    )
+VALUES
+    (
+        101,
+        'Real Madrid vs Barcelona',
+        'Champions League',
+        150.00,
+        'Available'
+    ),
+    (
+        102,
+        'Man City vs Liverpool',
+        'Premier League',
+        120.00,
+        'Selling Fast'
+    ),
+    (
+        103,
+        'Bayern Munich vs PSG',
+        'Champions League',
+        130.00,
+        'Available'
+    ),
+    (
+        104,
+        'AC Milan vs Inter Milan',
+        'Serie A',
+        90.00,
+        'Sold Out'
+    ),
+    (
+        105,
+        'Juventus vs Roma',
+        'Serie A',
+        80.00,
+        'Available'
+    );
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO BOOKINGS
 -- =========================================================================
-INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
-(501, 1, 101, 'A-12', 'Confirmed', 150.00),
-(502, 1, 102, 'B-04', 'Confirmed', 120.00),
-(503, 2, 101, 'A-13', 'Confirmed', 150.00),
-(504, 2, 101, NULL, NULL, 150.00),
-(505, 3, 102, 'C-20', 'Pending', 120.00);
-
-
+INSERT INTO
+    Bookings (
+        booking_id,
+        user_id,
+        match_id,
+        seat_number,
+        payment_status,
+        total_cost
+    )
+VALUES
+    (501, 1, 101, 'A-12', 'Confirmed', 150.00),
+    (502, 1, 102, 'B-04', 'Confirmed', 120.00),
+    (503, 2, 101, 'A-13', 'Confirmed', 150.00),
+    (504, 2, 101, NULL, NULL, 150.00),
+    (505, 3, 102, 'C-20', 'Pending', 120.00);
 
 -- =========================================================================
 -- Query 1
@@ -141,7 +218,6 @@ FROM
     bookings AS b
     INNER JOIN users AS u ON b.user_id = u.user_id
     INNER JOIN matches AS m ON b.match_id = m.match_id;
-
 
 -- =========================================================================
 -- Query 5
