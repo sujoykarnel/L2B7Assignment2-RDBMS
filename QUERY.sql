522df257c7752e90fba83aa6044fd52b4fba52bf
Sujoy Das <sujoykarnel@gmail.com>
2026-06-14 12:58:44 +0600

    users table created

--- QUERY.sql
+++ QUERY.sql
@@ -1,1 +1,10 @@
--- creates user table
+-- =========================================================================
+-- 1. CREATE USERS TABLE
+-- =========================================================================
+CREATE TABLE Users (
+    user_id serial primary key,
+    full_name varchar(100),
+    email varchar(150) unique,
+    role varchar(50) check (role in ('Ticket Manager', 'Football Fan'),
+    phone_number varchar(20)
+);
