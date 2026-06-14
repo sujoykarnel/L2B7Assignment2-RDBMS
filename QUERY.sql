ffd3df7805463a45f6510253229c46ba1e333ea5
Sujoy Das <sujoykarnel@gmail.com>
2026-06-14 13:12:17 +0600

    matches table added

--- QUERY.sql
+++ QUERY.sql
@@ -8,3 +8,15 @@ CREATE TABLE Users (
     role varchar(50) check (role in ('Ticket Manager', 'Football Fan'),
     phone_number varchar(20)
 );
+
+
+-- =========================================================================
+-- 2. CREATE MATCHES TABLE
+-- =========================================================================
+CREATE TABLE Matches (
+    match_id serial primary key,
+    fixture varchar(100),
+    tournament_category varchar(100),
+    base_ticket_price decimal(10,2) check (base_ticket_price >= 0) ,
+    match_status varchar(20) check (match_status in ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
